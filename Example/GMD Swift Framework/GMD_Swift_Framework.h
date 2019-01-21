07c981d9efe96c74ecea4dc5730f7a9590257b7b
Thomas Gmelig Meyling <thomas@youngmediaexperts.nl>
2019-01-21 16:33:49 +0100

    Add Carthage support

---
+++ Example/GMD Swift Framework/GMD_Swift_Framework.h
@@ -0,0 +1,21 @@
+//
+//  GMD_Swift_Framework.h
+//  GMD Swift Framework
+//
+//  Created by Thomas Gmelig Meyling on 21/01/2019.
+//  Copyright © 2019 Patrik Vaberer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMD_Swift_Framework.
+FOUNDATION_EXPORT double GMD_Swift_FrameworkVersionNumber;
+
+//! Project version string for GMD_Swift_Framework.
+FOUNDATION_EXPORT const unsigned char GMD_Swift_FrameworkVersionString[];
+
+#import " Google_Material_Design_Icons_Swift-Swift.h"
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMD_Swift_Framework/PublicHeader.h>
+
+
